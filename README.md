300a669f080a245150dd4305e183d75ebd91cd86
Glenn Welser <glenn.welser@gmail.com>
2017-01-17 14:33:36 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# preserve-acf-pro-license
-A WP must-use plugin to preserve your ACF Pro license info after migrating data via WP Migrate DB.
+# Preserve ACF Pro License
+
+*A WP must-use plugin to preserve your ACF Pro license info after migrating data via WP Migrate DB.*
+
+You know what stinks? Pulling down a WordPress database using (the excellent) WP Migrate DB Pro and then having to reenter the license info for ACF Pro! Wouldn't it be great if ACF Pro let you store your license info in `wp-config.php`, just like WP Migrate DB Pro let's you do? Sure would. But don't hold your breath. See this [ACF support thread that has been marked closed](https://support.advancedcustomfields.com/forums/topic/pro-license-key-in-config/).
+
+## It's dangerous to go alone! Take this.
+
+Save this plugin to `mu-plugins` and define your ACF Pro license in `wp-config.php` as shown below:
+
+~~~
+define( 'ACF_PRO_LICENSE', '<<LICENSE>>' );
+~~~
+
+Whenever WP Migrate DB Pro completes a migration, this plugin will fire off the function that updates your ACF Pro licnese.
+
+![Fist Bump](http://i3.kym-cdn.com/photos/images/original/000/357/107/14b.gif "Fist bump")
---
+++ preserve-acf-pro-license.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Plugin Name: Preserve ACF Pro License
+ * Plugin URI: http://clarknikdelpowell.com
+ * Description: A WP must-use plugin to preserve your ACF Pro license info after migrating data via WP Migrate DB.
+ * Version: 1.0.0
+ * Author: Glenn Welser
+ * Author URI: hhttp://clarknikdelpowell.com/agency/people/glenn
+ * License: GPL2
+ *
+ * Copyright (C) 2017  Glenn Welser  glenn@clarknikdelpowell.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+class Preserve_Acf_Pro_License {
+
+	/**
+	 * Setup WordPress hooks.
+	 *
+	 * @see https://deliciousbrains.com/tweaking-wp-migrate-db-pro-actions-filters/ WP Migrate DB Pro blog post about actions and filters.
+	 * @see https://github.com/deliciousbrains/wp-migrate-db-pro-tweaks/blob/master/wp-migrate-db-pro-tweaks.php#L27 Example plugin demonstrating available hooks and filters.
+	 *
+	 * @since 1.0.0
+	 */
+	public function run() {
+
+		add_action( 'wpmdb_migration_complete', array( $this, 'preserve_license' ) );
+
+	}
+
+	/**
+	 * Update ACF Pro license via ACF Pro function.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool true is value has changed, false if not or if update failed
+	 */
+	public function preserve_license() {
+
+		return acf_pro_update_license( ACF_PRO_LICENSE_KEY );
+
+	}
+
+}
+
+// Instantiate
+$eat_this_license_and_like_it = new Preserve_Acf_Pro_License();
+$eat_this_license_and_like_it->run();
